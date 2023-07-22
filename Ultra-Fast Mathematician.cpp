68c42a3c1562c03817a26d1740df8bbe4fc8c2b6
shubhamnsut <shubhamiit111@gmail.com>
2023-07-22 10:00:39 +0530

    first upload

---
+++ Ultra-Fast Mathematician.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b;
+    cin>>a>>b;
+
+    string ans ="";
+
+    for(int i=0;i<a.length();i++){
+        if(a[i]==b[i]){
+            ans+="0";
+        }
+        else{
+            ans+="1";
+        }
+    }
+
+    cout<<ans;
+return 0;
+}
